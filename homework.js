fee27d9405ecb40d1ab2b86faf56fa8054b2c220
Isaac Obuor <iobuor88@gmail.com>
2024-11-06 06:11:00 +0000

    first commit

--- homework.js
+++ homework.js
@@ -37,6 +37,73 @@ false;
 "5" == 5;
 null == undefined;
 "5" === 5;
+null === undefined;
+"This is a string".charAt(0);
+"Hello world".substring(0,5);
+"Hello".length;
+null;
+undefined;
+var someVar = 5;
+some0therVar = 10;
+var someThirdVar;
+var somefourthVar = 2, somefifthVar = 4;
+someVar += 5;
+someVar *= 10;
+someVar++;
+someVar--;
+var myArray = ["Hello", 45, true];
+myArray[1];
+myArray.push("world");
+myArray.length;
+myArray[3] = "Hello";
+myArray.unshift(3);
+someVar = myArray.shift();
+myArray.push(3);
+someVar = myArray.pop();
+var myArray0 = [32,false,"js",12,56,90];
+myArray0.join(";");
+myArray0.slice(1,4);
+myArray0.splice(2,4,"hi","wr","ld");
+var my0bj = {key1: "Hello", key2: "world"};
+var my0bj = {mykey: "myValue", "my other key": 4};
+my0bj["my other key"];
+my0bj.mykey;
+my0bj.myThirdKey = true;
+my0bj.myFourthKey;
+var count = 1;
+if (count == 3){
+    
+} else if (count == 4){
+    
+} else {
+    
+}
+while (true){
+    
+}
 
+var InputEvent;
+do {
+    input = getInput();
+} while (!isValid(input));
+for (var i = 0; i < 5; i++){
+    
+}
 
-   
+outer:
+for (var i = 0; i < 10; i++) {
+    for (var j = 0; < 10; j++) {
+        if (i == 5 && j ==5) {
+            break outer;
+        }
+    }
+}
+
+var description = "";
+var person = {fname: "paul", lname:"Ken", age:18};
+for (var x in person){
+    description += person[x] + " ";
+}
+
+var myPets = "";
+var pets = ["cat", "dog", "hamster", "hedgehog"];
